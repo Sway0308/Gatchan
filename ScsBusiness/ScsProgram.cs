e75cb776508d8f18741b142fcad8f7c870c20d4d
Sway0308 <sway0308@gmail.com>
2018-10-19 14:09:38 +0800

    New Push

---
+++ ScsBusiness/ScsProgram.cs
@@ -0,0 +1,24 @@
+﻿using ScsService;
+using System;
+using System.Threading.Tasks;
+
+namespace ScsBusiness
+{
+    public class ScsProgram
+    {
+        public ScsProgram(string url, string companyID, string userID, string password)
+        {
+            this.ScsService = Connect(url, companyID, userID, password).GetAwaiter().GetResult();
+        }
+
+        private SCSServiceSoapClient ScsService { get; }
+
+        private async Task<SCSServiceSoapClient> Connect(string url, string companyID, string userID, string password)
+        {
+
+            var client = new SCSServiceSoapClient(SCSServiceSoapClient.EndpointConfiguration.SCSServiceSoap, url);
+            var result = await client.LoginAsync(new TWsLoginInputArgs { CompanyID = companyID, UserID = userID, Password = password });
+            
+        }
+    }
+}
